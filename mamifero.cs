6dd74dbc1ca7608849680421fe620d4972328ee3
martha <marthademarcos16@gmail.com>
2024-03-13 08:50:31 -0600

    2005

---
+++ mamifero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciomdm
+{
+    internal class mamifero
+    {
+        private string nombre { get; set; }
+        protected double peso { get; set; }
+
+        //Constructor
+        public mamifero(string n, double p)
+        {
+            this.nombre = n;
+            this.peso = p;
+        }
+
+        //Metodos
+        public void setNombre(string n)
+        {
+            nombre = n;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+    }
+}
